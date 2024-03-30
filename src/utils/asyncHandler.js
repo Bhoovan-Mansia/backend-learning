f04b63f6a09a5ed7774187a8459a1478308ec797
Bhoovan-Mansia <bhoovanmansia9@gmail.com>
2024-03-30 13:53:46 +0530

    utils code added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+/* promise based syntax
+cosnt asyncHandler = (reqHandler) => {
+    (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+} 
+*/
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+export default asyncHandler 
