3033736d129625f0d9ac38756d8f0aa2d37c34cc
Bhoovan-Mansia <bhoovanmansia9@gmail.com>
2024-06-08 12:00:50 +0530

    Apierror response msg changes.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -190,7 +190,7 @@ const changeCurrentPassword = asyncHandler(async (req,res) => {
 const getCurrentUser = asyncHandler(async (req,res) => {
      return res
      .status(200)
-     .json(200, req.user, "Current user fetched successfully");
+     .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req,res) => {
@@ -238,6 +238,8 @@ const updateUserAvatar = asyncHandler(async (req,res) => {
           },
           {new: true}
      ).select("-password");
+
+     //TODO -> delete old image;
 
      return res
      .status(200)
